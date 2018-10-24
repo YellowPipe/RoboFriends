d4440833523ff5e40b71220d77cb785cc6cc9db6
Philipp <philippdmitrienko@gmail.com>
2018-10-24 15:07:39 +0200

    uploading icons

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ searchfield, searchChange} ) => {
 	return (
 		<div className='pa2'>
 			<input
+			aria-label='Search robots'
 			className='pa3 yellow ba b--green bg-mid-gray'
 			type='search'
 			placeholder='search robots'
